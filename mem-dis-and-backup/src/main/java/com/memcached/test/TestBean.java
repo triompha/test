ab86fe8f172610f82da026c5b2b1292f433106f0
triompha <triompha@163.com>
2013-11-06 18:57:12 +0800

    将测试过的代码统一整理到一个工程

--- mem-dis-and-backup/src/main/java/com/memcached/test/TestBean.java
+++
@@ -1,9 +0,0 @@
-package com.memcached.test;
-
-public class TestBean {
-	private final String a ="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-	private final String b = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxeeeeeeee";
-	private final String c = "ffffffffffffffffffffffffffffffffffffffffffffffffffff";
-	private final String m ="xxxxxdfffffffffffffffffffffffwwwwwwwwwwwwwwwwwwwwwwww";
-
-}
